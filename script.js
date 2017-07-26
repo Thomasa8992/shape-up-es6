e97b29dbdb0e8c3163453758c98959b4e2b48093
InnovateBham_6 <InnovateBham_6@admins-iMac.local>
2017-07-26 17:05:20 -0500

    Added functionality for constructors

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
-alert();
+document.addEventListener("DOMContentLoaded", function(){
+class Shapes {
+  constructor(shapeName, height, width, radius, area, perimeter) {
+        this.shapeName = shapeName;
+        this.height = height;
+        this.width = width;
+        this.radius = radius;
+        this.area = area;
+        this.perimeter = perimeter;
+  }
+  
+  shapeNames() {
+      console.log(this.shapeName);
+  }
+}
+
+class Circle extends Shapes {
+  constructor(radius) {
+        super("cirlce", "NA","NA", 4 ,"NA","NA");
+  }
+}
+
+class Square extends Shapes {
+    constructor() {
+      super();
+  }
+}
+
+class Rectangle extends Shapes {
+  constructor() {
+        super("cirlce", "NA","NA","NA","NA");
+  }
+}
+
+class Triangle extends Shapes {
+  constructor(brand) {
+        super(2, brand);
+  }
+}
+
+var circle1 = new Circle();
+console.log(circle1);
+circle1.shapeNames();
+});
