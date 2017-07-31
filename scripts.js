21d454249cd917a47f122dc3e9160f2d4230780e
InnovateBham_6 <InnovateBham_6@admins-iMac.local>
2017-07-31 09:59:21 -0500

    added some extra content

--- scripts.js
+++ scripts.js
@@ -175,3 +175,10 @@ if(element !== " "){
     element.value = "";
     }
 }
+
+function isNumberKey(evt){
+    var charCode = (evt.which) ? evt.which : event.keyCode
+    if (charCode > 31 && (charCode < 48 || charCode > 57))
+        return false;
+    return true;
+}
